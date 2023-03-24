88c63ebce0dc4a689617495d9fc3013767ef5df7
Aleks Rutins <aleks@rutins.com>
2023-03-24 09:20:55 -0400

    Day 2, part 2

--- app/src/main/java/aoc22/Day2.java
+++ app/src/main/java/aoc22/Day2.java
@@ -3,12 +3,13 @@ package aoc22;
 import java.util.stream.Stream;
 
 public class Day2 implements Exercise {
-    private record Round(int opp, int me) {}
+    private record Round(int opp, int me) {
+    }
 
     private Stream<Round> parseInput(String input) {
         return input.lines().filter(line -> !line.isEmpty()).map(line -> new Round(
-                line.split(" ")[0].charAt(0) - 'A',
-                line.split(" ")[1].charAt(0) - 'X'
+            line.split(" ")[0].charAt(0) - 'A',
+            line.split(" ")[1].charAt(0) - 'X'
         ));
     }
 
@@ -17,14 +18,25 @@ public class Day2 implements Exercise {
     }
 
     private int pointsFor(Round round) {
-        if(round.opp == round.me) return pointsFor(round.me) + 3;
-        else if(round.opp == 0 && round.me == 1) return pointsFor(1) + 6;
-        else if(round.opp == 1 && round.me == 2) return pointsFor(2) + 6;
-        else if(round.opp == 2 && round.me == 0) return pointsFor(0) + 6;
+        if (round.opp == round.me) return pointsFor(round.me) + 3;
+        else if (round.opp == 0 && round.me == 1) return pointsFor(1) + 6;
+        else if (round.opp == 1 && round.me == 2) return pointsFor(2) + 6;
+        else if (round.opp == 2 && round.me == 0) return pointsFor(0) + 6;
         else return pointsFor(round.me);
     }
 
+    private int pointsForPt2(Round round) {
+        int me = switch (round.me) {
+            case 0 -> (round.opp + 2) % 3; // loss
+            case 1 -> round.opp; // draw
+            case 2 -> (round.opp + 4) % 3; // win
+            default -> 0;
+        };
+        return pointsFor(me) + (round.me * 3);
+    }
+
     public void run(String input) {
-        System.out.println("Score: " + parseInput(input).map(this::pointsFor).reduce(0, Integer::sum));
+        System.out.println("Score (Part 1): " + parseInput(input).map(this::pointsFor).reduce(Integer::sum));
+        System.out.println("Score (Part 2): " + parseInput(input).map(this::pointsForPt2).reduce(Integer::sum));
     }
 }
