be3fff11dafe4715fc93d2ba309393f242e4f4cc
Aleks Rutins <aleks@rutins.com>
2023-05-03 10:37:01 -0400

    Day 6, part 2

--- app/src/main/java/aoc22/Day6.java
+++ app/src/main/java/aoc22/Day6.java
@@ -30,5 +30,21 @@ search: for(int i = 0; i < fullBuf.length; i++) {
             System.out.println("Part 1: " + (i + 1));
             break;
         }
+
+        buf = new char[14];
+search: for(int i = 0; i < fullBuf.length; i++) {
+            buf = Arrays.copyOfRange(buf, 1, 15);
+            buf[13] = fullBuf[i];
+            if(buf[0] == 0) continue;
+            for(int j = 0; j < 14; j++) {
+                for(int k = 0; k < 14; k++) {
+                    if(buf[j] == buf[k] && j != k) {
+                        continue search;
+                    }
+                }
+            }
+            System.out.println("Part 2: " + (i + 1));
+            break;
+        }
     }
 }
